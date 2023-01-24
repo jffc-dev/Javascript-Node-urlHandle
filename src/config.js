5f1df274d729f955f4bca151b2919fa244c95e83
Javier Flores <jffc.dev@gmail.com>
2023-01-24 06:44:39 -0500

    publish(App): Env variables file changed

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ export const config = {
   dev: process.env.NODE_ENV !== 'production',
   port: process.env.PORT || 3000,
   mongo: {
-    uri: 'mongodb://127.0.0.1:27017/',
-    db: 'manejoCadenas'
+    uri: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/',
+    db: process.env.MONGO_DB || 'manejoCadenas'
   }
 }
