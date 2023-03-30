ea1046f683bf0bea6269fae6d22a00ee6a0a4de3
Javier Flores <jffc.dev@gmail.com>
2023-03-30 00:24:53 -0500

    feat(App): validationHandler function improved

--- src/utils/middlewares/validationHandler.js
+++ src/utils/middlewares/validationHandler.js
@@ -3,6 +3,8 @@
  * @param {import('joi').Schema schema}
  */
 
+import { AppResponse } from '../general'
+
 const validate = (data, schema) => {
   const { error } = schema.validate(data)
   return error
@@ -14,15 +16,20 @@ const validationHandler = (schema, check = 'body') => {
     if (err) {
       const { details: [error] } = err
       const { type, context } = error
-      res.status(406).json({
-        status: 0,
-        msg: { type, context },
-        rpta: null
-      })
+      const errorMessage = buildMessage(type, context)
+      const rsp = new AppResponse(0, errorMessage, null)
+      res.status(406).json(rsp)
     } else {
       next()
     }
   }
+}
+
+const buildMessage = (type, context) => {
+  if (type === 'string.empty') {
+    return `One of the ${context.key} entries was empty.`
+  }
+  return 'An error occurred in the validation of entries'
 }
 
 export default validationHandler
