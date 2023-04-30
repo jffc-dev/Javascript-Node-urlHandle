245eff5a985a354f78e799285f54bb30d5917e69
Javier Flores <jffc.dev@gmail.com>
2023-04-29 23:39:36 -0500

    feat(App): controller for playlist updated

--- src/components/playlist/controller.js
+++ src/components/playlist/controller.js
@@ -1,6 +1,8 @@
 import MongoPlaylistRepository from './infraestructure/MongoPlaylistRepository.js'
 import createPlaylistRepo from './application/createPlaylist.js'
 import getAllPlaylistRepo from './application/getAllPlaylist.js'
+import getPlaylistRepo from './application/getPlaylist.js'
+import createPlaylistWithListRepo from './application/createPlaylistWithListRepo.js'
 import { AppResponse } from '../../utils/general.js'
 
 const PlaylistRepository = new MongoPlaylistRepository()
@@ -16,11 +18,35 @@ export const createPlaylist = async (req, res, next) => {
   }
 }
 
-export const getAllPlaylist = async (req, res, next) => {
+export const getAllPlaylists = async (req, res, next) => {
   try {
     const query = getAllPlaylistRepo({ PlaylistRepository })
     const { playlists } = await query()
     const rsp = new AppResponse(1, 'Playlist was successfully listed.', playlists)
+    res.status(201).json(rsp)
+  } catch (e) {
+    next(e)
+  }
+}
+
+export const getPlaylist = async (req, res, next) => {
+  try {
+    const query = getPlaylistRepo({ PlaylistRepository })
+    const rpta = await query({ id: req.params.id })
+    console.log(rpta.links)
+    const rsp = new AppResponse(1, 'Playlist was successfully listed.', rpta)
+    res.status(201).json(rsp)
+  } catch (e) {
+    next(e)
+  }
+}
+
+export const createPlaylistWithList = async (req, res, next) => {
+  try {
+    const query = createPlaylistWithListRepo({ PlaylistRepository })
+    const { links } = req.body
+    const data = await query({ links })
+    const rsp = new AppResponse(1, 'Playlist was successfully listed.', data)
     res.status(201).json(rsp)
   } catch (e) {
     next(e)
