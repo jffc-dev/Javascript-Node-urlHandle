c03cb98c16b76927043ed96257bc29018103e1d0
Javier Flores <jffc.dev@gmail.com>
2023-05-01 23:50:11 -0500

    feat(App): get and addWithList functions added to playlist repository

--- src/components/playlist/infraestructure/MongoPlaylistRepository.js
+++ src/components/playlist/infraestructure/MongoPlaylistRepository.js
@@ -1,4 +1,8 @@
 import MongoLib from '../../../lib/mongo.js'
+import MongoUrlRepository from '../../url/infraestructure/MongoUrlRepository.js'
+import pckMongo from 'mongodb'
+
+const { ObjectId } = pckMongo
 
 class MongoPlaylistRepository {
   constructor () {
@@ -18,12 +22,20 @@ class MongoPlaylistRepository {
 
   async get (id) {
     const playlist = await this.mongoDB.get(this.collection, id)
-    return { playlist }
+    const UrlRepository = new MongoUrlRepository()
+    const formatLinks = playlist.links.map((link) => ObjectId(link))
+    const { urls: links } = await UrlRepository.getByArray('_id', formatLinks)
+    return { ...playlist, links }
   }
 
   async getAll () {
     const playlists = await this.mongoDB.getAll(this.collection)
     return { playlists }
+  }
+
+  async addWithList (links) {
+    const id = await this.mongoDB.create(this.collection, { links })
+    return { id, links }
   }
 }
 
