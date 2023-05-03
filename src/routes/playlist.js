15e3c4b37dac192dc52694d32d6ed726eb35def9
Javier Flores <jffc.dev@gmail.com>
2023-05-02 21:41:56 -0500

    feat(App): playlist routes added

--- src/routes/playlist.js
+++ src/routes/playlist.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import {
   createPlaylist,
-  getAllPlaylist
+  getAllPlaylists,
+  createPlaylistWithList,
+  getPlaylist
 } from '../components/playlist/controller.js'
 import validationHandler from '../utils/middlewares/validationHandler.js'
 import { createPlaylistSchema } from '../components/playlist/domain/add.js'
@@ -9,6 +11,8 @@ import { createPlaylistSchema } from '../components/playlist/domain/add.js'
 const router = express.Router()
 
 router.post('/', validationHandler(createPlaylistSchema), createPlaylist)
-router.get('/', getAllPlaylist)
+router.get('/', getAllPlaylists)
+router.get('/:id', getPlaylist)
+router.post('/save-list', createPlaylistWithList)
 
 export default router
